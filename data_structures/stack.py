351e12c926feddb25d3747801595b8787b080403
Naman Thanki <stuniz7855@gmail.com>
2024-07-09 00:25:55 +0530

    MAJOR BUG FIXED - Resize method should check for unexpected circumstances where entered new_capacity is less than 1 - should update length properly take min of length and capacity to keep it within range

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -7,21 +7,23 @@ class Stack:
         self.data: list[Any] = [None] * capacity
         
     def resize(self, new_capacity: int) -> None:
+        if new_capacity < 1:
+            new_capacity = 1
         new_data: list[Any] = [None] * new_capacity
         for i in range(self.length):
             new_data[i] = self.data[i]
         self.data = new_data
         self.capacity = new_capacity
+        self.length = min(self.length, self.capacity)
     
     def push(self, value: Any) -> None:
-        if self.length == self.capacity:
+        if self.is_full():
             self.resize(self.capacity * 2)
-            
         self.data[self.length] = value
         self.length += 1
         
     def pop(self) -> Any:
-        if self.length == 0:
+        if self.is_empty():
             raise IndexError('Stack underflow')
         item = self.data[self.length - 1]
         self.length -= 1
@@ -30,9 +32,15 @@ class Stack:
         return item
 
     def peek(self) -> Any:
-        if self.length == 0:
+        if self.is_empty():
             raise IndexError('Stack underflow')
         return self.data[self.length - 1]
+    
+    def is_empty(self) -> bool:
+        return self.length == 0
+    
+    def is_full(self) -> bool:
+        return self.length == self.capacity
     
     def __str__(self) -> str:
         return str(self.data[:self.length])
@@ -73,4 +81,4 @@ def test_stack():
         assert str(e) == 'Stack underflow'
         print(str(e))
     
-test_stack()
+# test_stack()
